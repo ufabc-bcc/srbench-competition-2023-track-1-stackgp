12e1c6573a356de479c7e44945935d506b61d357
hoolagans <pizzahaut@gmail.com>
2023-06-29 14:50:23 -0400

    started report

--- src/dataset_1_fitting.py
+++ src/dataset_1_fitting.py
@@ -12,8 +12,8 @@ import pandas as pd
 Z = np.loadtxt(f"datasets/dataset_1.csv", delimiter=",", skiprows=1)
 X, y = Z[:, :-1], Z[:, -1]
 
-models=sgp.evolve(X.transpose(), y, generations=4000, ops=sgp.allOps(), elitismRate=10,tourneySize=5,popSize=300)
-#models=sgp.activeLearning(X.transpose(), y,200)
+#models=sgp.evolve(X.transpose(), y, generations=4000, ops=sgp.allOps(), elitismRate=10,tourneySize=5,popSize=300)
+models=sgp.activeLearning(X.transpose(), y,200)
 print(sgp.printGPModel(models[0]))
 print(sgp.fitness(models[0],X.transpose(), y))
 fits=[sgp.fitness(i, X.transpose(), y) for i in models]
